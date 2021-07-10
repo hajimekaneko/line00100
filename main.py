c2f453d6ca360223759cfc73532eff017a0c052d
hajimekaneko <hajime.10600@gmail.com>
2021-07-10 15:37:12 +0900

    'test'

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ from linebot.models import (
 app = Flask(__name__)
 
 #環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["HYqFu+Xyf72EwDpaPzjNzPy46FJ4Jk7c3ViXdyGQbCxf0XEEfdLb/QUv9HYyxQEpH0CZvS/S7p/B2R4VsilzIO0scAG0QYHTEndSAXJE9QNEjxkLlEqM1EuQrOUhGpglWP9VXSgeK95cMzBZ1ZsoEgdB04t89/1O/w1cDnyilFU="]
-YOUR_CHANNEL_SECRET = os.environ["385a2ceff0bf70423ed74c66b6df061d"]
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
